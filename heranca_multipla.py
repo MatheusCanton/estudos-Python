7352bb6585de610ed5baeda31f9e7b1d80987cee
Matheus Platero Canton <mp.canton@hotmail.com>
2019-02-18 15:20:36 -0300

    Add files via upload

---
+++ heranca_multipla.py
@@ -0,0 +1,49 @@
+#AULA DE HERANÇA CURSO DE ORIENTAÇÃO A OBJETO DA ALURA
+
+class Funcionario:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def registra_horas(self, horas):
+        print('Horas registradas.')
+
+    def mostrar_tarefas(self):
+        print('Fez muita coisa...')
+
+class Caelum(Funcionario):
+    def mostrar_tarefas(self):
+        print('Fez muita coisa, Caelumer')
+
+    def busca_cursos_do_mes(self, mes=None):
+        print(f'Mostrando cursos - {mes}' if mes else 'Mostrando cursos desse mês')
+
+class Alura(Funcionario):
+    def mostrar_tarefas(self):
+         print('Fez muita coisa, Alurete!')
+
+    def busca_perguntas_sem_resposta(self):
+        print('Mostrando perguntas não respondidas do fórum')
+
+class Hipster:
+    def __str__(self):
+        return f'Hipster,  {self.nome}'
+
+class Junior(Alura):
+    pass
+
+class Pleno(Alura, Caelum, Hipster):
+    pass
+
+
+
+matheus = Junior('Matheus')
+matheus.busca_perguntas_sem_resposta()
+matheus.mostrar_tarefas()
+
+greg = Pleno('Greg')
+greg.busca_perguntas_sem_resposta()
+greg.busca_cursos_do_mes()
+
+greg.mostrar_tarefas()
+
+print(greg)
