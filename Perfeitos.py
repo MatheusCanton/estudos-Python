01182adf52480cbbe37b3cec325600c9b1646b7d
Matheus Platero Canton <mp.canton@hotmail.com>
2019-03-06 13:04:33 -0300

    Update Perfeitos.py

--- Perfeitos.py
+++ Perfeitos.py
@@ -1,5 +1,6 @@
+#lista de perfeitos para usar junto com o codigo perfeitos_test
 
-def divisores(n):
+def lista_divisores(n):
     lista = []
     for i in range(1, n//2+1):
         if n % i == 0:
@@ -8,7 +9,7 @@ def divisores(n):
 
 
 def eh_perfeito(n):
-    lista = divisores(n)
+    lista = lista_divisores(n)
     soma = sum(lista)
     if soma == n:
         return True
@@ -18,10 +19,22 @@ def eh_perfeito(n):
 
 def lista_perfeitos(n):
     lista = []
-    for i in range(2, n):
+    for i in range(2, n+1):
         if eh_perfeito(i):
             lista.append(i)
     return lista
 
 
+def lista_n_perfeitos(n):
+    x = 0
+    lista = []
+    while x < n :
+        for i in range(1,x+1):
+            if eh_perfeito(i):
+                lista.append(i)
+                x +=1
+    return lista
+
 print(lista_perfeitos(10000))
+
+#instale pytest e verifique se o codigo está correto.
